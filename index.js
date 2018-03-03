a2b4b69f481db8bda3a8dfa2ec23a49ec81833f8
luyisheng <luyisheng@bytedance.com>
2018-03-03 10:20:45 +0800

    fix:change const to var
    Change-Id: I46261d2775513fe313bb52093f9270a203d0c35c

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cleanSketch = require('clean-sketch');
+var cleanSketch = require('clean-sketch');
 module.exports = function(source) {
   source = cleanSketch(source.toString());
   return source;
